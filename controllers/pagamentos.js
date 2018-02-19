405b314fdb27dd470926e3a3dcd365d376ae046b
Acauã Pitta <acaua.pitta@hotmail.com>
2018-02-19 17:00:42 -0300

    Pagamento Criado - aula 03

---
+++ controllers/pagamentos.js
@@ -0,0 +1,25 @@
+module.exports = function(app){
+	app.get('/pagamentos', function(req, res){
+		console.log('Recebida requisicao de teste na porta 3000.')
+		res.send('OK.');
+	});
+
+
+	app.post('/pagamentos/pagamento', function(req, res){
+		
+		var pagamento = req.body;
+		console.log('processando requisicao pagamento');
+		
+		pagamento.status = 'Criado';
+		pagamento.data = new Date();
+
+		var connection = app.persistencia.connectionFactory();
+		var pagamentoDao = new app.persistencia.PagamentoDao(connection); 
+
+		pagamentoDao.salva(pagamento, function(erro, resultado){
+			console.log('pagamento criado!');
+			res.json(pagamento);
+		});
+
+	});
+}
---
+++ persistencia/PagamentoDao.js
@@ -0,0 +1,22 @@
+function PagamentoDao(connection) {
+    this._connection = connection;
+}
+
+PagamentoDao.prototype.salva = function(pagamento,callback) {
+    this._connection.query('INSERT INTO pagamentos SET ?', pagamento, callback);
+}
+
+PagamentoDao.prototype.lista = function(callback) {
+    this._connection.query('select * from pagamentos',callback);
+}
+
+PagamentoDao.prototype.buscaPorId = function (id,callback) {
+    this._connection.query("select * from pagamentos where id = ?",[id],callback);
+}
+
+module.exports = function(){
+    return PagamentoDao;
+};
+
+//prototype -> garantir que cada vez que invoque uma nova instancia dele:
+//consiga invocar metodos exclusivos - para nao colidir trheads
